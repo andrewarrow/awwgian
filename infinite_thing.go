3acf2c07eed7e97da72f0ed7d87431f0f6e94dc3
Andrew Arrow <andrew@bird.co>
2018-12-21 07:10:11 +0000

    progress

--- infinite_thing.go
+++ infinite_thing.go
@@ -4,8 +4,6 @@ import "fmt"
 import "time"
 
 type InfiniteThing struct {
-	waitFactor int
-	canFork    bool
 }
 
 func (t *InfiniteThing) existForInfinity() {
@@ -13,20 +11,4 @@ func (t *InfiniteThing) existForInfinity() {
 		fmt.Printf("i am e\n")
 		time.Sleep(time.Second * 1)
 	}
-}
-
-func (t *InfiniteThing) init() {
-	t.waitFactor = 1
-}
-
-func (t *InfiniteThing) run() {
-	t.init()
-	for {
-		fmt.Printf("i am e\n")
-		time.Sleep(time.Second * time.Duration(t.waitFactor))
-	}
-}
-func (t *InfiniteThing) firstFork() {
-	fmt.Printf("setting limit on forking off...")
-	t.canFork = true
 }
